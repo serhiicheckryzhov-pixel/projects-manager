e3361a9132dc14a9a4ebfafd26db80da703d7be1
SECH <sc@belisar.de>
2026-08-11 23:42:09 +0300

    Laravel Pint test

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -22,7 +22,7 @@ class UserController extends ApiController
     /**
      * Show the form for creating a new resource.
      */
-    public function create()     {
+    public function create()  {
         //
     }
 
